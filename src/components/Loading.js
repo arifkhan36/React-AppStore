26b56ab107e408566af30e8a4f9e1e6bd4690a23
md khan <arifk36@yahoo.com>
2018-06-20 03:42:45 -0400

    fixed logo and added menu

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Loading.css';
 
 const Loading = () => (
-<div className="sk-folding-cube">
-  <div className="sk-cube1 sk-cube"></div>
-  <div className="sk-cube2 sk-cube"></div>
-  <div className="sk-cube4 sk-cube"></div>
-  <div className="sk-cube3 sk-cube"></div>
-</div>
+   <div>
+        <div className="sk-folding-cube">
+       <div className="sk-cube1 sk-cube"></div>
+      <div className="sk-cube2 sk-cube"></div>
+      <div className="sk-cube4 sk-cube"></div>
+       <div className="sk-cube3 sk-cube"></div>
+     </div>
+    <h1>once circle disappear begin scrolling!!!!!!!</h1>
+
+    </div>
+
 
 )
 export default Loading;
